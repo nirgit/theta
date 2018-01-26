2ed77e81aba145118fb7b28215076cf78564027e
Nir Moav <nirm@wix.com>
2018-01-26 13:52:28 +0200

    an ultra simple example of a working runner

---
+++ examples/example.js
@@ -0,0 +1,7 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+it('should calculate the same of two numbers', function() {
+    expect(1 + 2).to.equal(3);
+});
+
--- theta.js
+++ theta.js
@@ -1,2 +1,36 @@
 #! /usr/bin/env node
-console.log('This is the Theta');
+console.log('This is Theta - A modern Javascript Test Runner');
+
+function initThetaGlobals(testsListToInit) {
+    global.it = function(description, test) {
+        testsListToInit.push({description, test});
+    };
+}
+
+function Theta (filesToRun) {
+    filesToRun = filesToRun || [];
+    filesToRun = [].concat(filesToRun);
+    const tests = [];
+
+    initThetaGlobals(tests);
+
+    return {
+        run() {
+            console.log('running tests!', filesToRun);
+            filesToRun.forEach(file => {
+                require(file);
+            });
+            tests.forEach(t => {
+                try {
+                    t.test();
+                    console.log(`ok ${t.description}`);
+                } catch(e) {
+                    console.log(`not ok ${t.description}`, e.stack);
+                }
+            })
+        }
+    };
+}
+
+const filesToRun = ['./examples/example.js'];
+Theta(filesToRun).run();
